6e806b5741693c4428ef11eddfd112f0a6e1ca34
nefertiajj@sbcglobal.net <>
2014-08-14 19:44:32 +0000

    Init Commit

--- js/main.js
+++ js/main.js
@@ -46,6 +46,10 @@ var mainState = {
     //Add in pipes over 1.5 seconds to the screen
     this.timer = game.time.events.loop(1500, this.addRowOfPipes ,this);
     
+    this.score = 0;
+    
+    this.labelScore = game.add.text(20, 20, "0",{font: "30px Arial", fill:"#ffffff"});
+    
     //When spacebar is pressed, make the bird jump!
     var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     spaceKey.onDown.add(this.jump, this);
@@ -59,6 +63,7 @@ var mainState = {
     if (this.bird.inWorld == false) {
       this.restartGame();
 }
+  game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
 },
 
   addOnePipe: function (x,y) {
@@ -87,6 +92,8 @@ var mainState = {
       this.addOnePipe(400, i*60 + 10);
   
   }   
+    this.score += 1;
+    this.labelScore.text = this.score
 },
 
   jump: function () {
